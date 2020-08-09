d7270030f686186d38006c422e452f1c087b5050
Issac Angulo <issac.angulo@gmail.com>
2020-08-09 02:49:28 -0700

    server setup

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+module.exports = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} : ${moment().format()}`);
+    next();
+};
+
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Hola Mundo!!!!');
+const express = require('express');
+const logger = require('./middleware/logger');
+const pages = require('./routes/public/pages');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(logger);
+app.use(pages);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

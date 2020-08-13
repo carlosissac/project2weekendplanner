36625b5b85bd3b8155b44cb57a9e8c9854a6333b
karynclarke <karynclarke@comcast.net>
2020-08-13 11:50:39 -0700

    tailwindupdates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,25 @@ module.exports = {
     },
     variants: {},
     plugins: [],
+    extends: ['stylelint-config-recommended']
+
+};
+
+module.exports = {
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'tailwind',
+                    'apply',
+                    'variants',
+                    'responsive',
+                    'screen',
+                ],
+            },
+        ],
+        'declaration-block-trailing-semicolon': null,
+        'no-descending-specificity': null,
+    },
 };
